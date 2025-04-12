22099a9a029c312bee9f7e7b8d246bffddecd7e1
zivs <zivs190208@gmail.com>
2025-04-12 12:59:18 +0300

    made the un encrypted settings file, working on encryption for the file.

--- Anigma_cpp/Gear.cpp
+++ Anigma_cpp/Gear.cpp
@@ -1,26 +1,32 @@
 #include "Gear.h"
 
-Gear::Gear()
+Gear::Gear(gearType type)
 {///initializing the gear to the default starting state. 
+	this->_type = type;
 	for (int let = 0; let < LETTERS; let++)
 	{
 		_interface[let][0] = static_cast<char>('a' + let);
-		_interface[let][0] = static_cast<char>('a' + let);
+		_interface[let][1] = static_cast<char>('a' + let);
 	}
+}
+
+Gear::Gear(const std::string& filePath, const std::string& password)
+{
+	const std::string settings = decryptSettings(password);
+
 }
 
 Gear::~Gear()
 //takes the gear and lodes it in the file in the anigma format
 {
-	this->close();
+	//this->close();
 }
 
 void Gear::rotate()
 {
 	for (int let = 0; let < LETTERS; let++)
 	{
-		_interface[let][0] = static_cast<char>(_interface[let][0] + (let % 26));
-		_interface[let][0] = static_cast<char>(_interface[let][0] + (let % 26));
+		_interface[let][0] = (_interface[let][0] - 'a' + 1) % 26 + 'a';
 	}
 }
 
@@ -30,6 +36,17 @@ void Gear::printGearState()
 	for (int let = 0; let < LETTERS; let++)
 	{
 		std::cout << let + 1 << ". " << _interface[let][0];
-		std::cout << ", " << _interface[let][0] << std::endl;
+		std::cout << ", " << _interface[let][1] << std::endl;
 	}
 }
+
+const std::string Gear::encryptSettings(const std::string& password)
+{
+	
+}
+
+const std::string Gear::decryptSettings(const std::string& password)
+{
+	
+}
+
--- Anigma_cpp/Gear.h
+++ Anigma_cpp/Gear.h
@@ -1,20 +1,28 @@
-#pragma once]
+#pragma once
 #include <iostream>
 #include <string>
 
 #define LETTERS 26
 
+enum gearType
+{
+	first, second, third
+};
+
 class Gear
 {
 public:
-	Gear();
-	Gear(const std::string& filePath);
+	Gear(gearType type);
+	Gear(const std::string& filePath, const std::string& password);
 	~Gear();
-	const std::string close();
+	//const std::string close();
 	void rotate();
+	void printGearState();///function to help debuging delete when every thing works
 private:
 	//26 rows for the 26 letters first colom is input and second in output.
+	gearType _type;
 	char _interface[26][2];
-	void printGearState();///private function to help debuging.
+	const std::string encryptSettings(const std::string& password);
+	const std::string decryptSettings(const std::string& password);
 };
 
