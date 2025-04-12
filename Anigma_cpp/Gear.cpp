87fa394a8e9528f1677729957e09a6791c2120cf
zivs <zivs190208@gmail.com>
2025-04-12 14:57:49 +0300

    stoped with the encryption for now, continuing with the anigma itself

--- Anigma_cpp/Gear.cpp
+++ Anigma_cpp/Gear.cpp
@@ -1,8 +1,13 @@
 #include "Gear.h"
+#include <sstream>
+#include <fstream>
 
-Gear::Gear(gearType type)
+
+
+Gear::Gear(const std::string& filePath, gearType type)
 {///initializing the gear to the default starting state. 
 	this->_type = type;
+	this->settingsFile = filePath;
 	for (int let = 0; let < LETTERS; let++)
 	{
 		_interface[let][0] = static_cast<char>('a' + let);
@@ -12,14 +17,17 @@ Gear::Gear(gearType type)
 
 Gear::Gear(const std::string& filePath, const std::string& password)
 {
-	const std::string settings = decryptSettings(password);
+	//const std::string settings = decryptSettings(password);
 
 }
 
 Gear::~Gear()
 //takes the gear and lodes it in the file in the anigma format
 {
-	//this->close();
+	const std::string gearSettings = getGearSettings();
+	std::fstream file(this->settingsFile);
+	file << gearSettings;
+	file.close();
 }
 
 void Gear::rotate()
@@ -42,11 +50,28 @@ void Gear::printGearState()
 
 const std::string Gear::encryptSettings(const std::string& password)
 {
-	
+	const std::string planeSettings = getGearSettings();
+
 }
 
 const std::string Gear::decryptSettings(const std::string& password)
 {
 	
+}
+
+const std::string Gear::getGearSettings()
+{
+	std::stringstream settingsStream;
+	settingsStream << _type << "{";
+	for (int i = 0; i < LETTERS; i++)
+	{
+		if (i != LETTERS - 1)
+		{
+			settingsStream << _interface[i][0] << ":" << _interface[i][1] << ", ";
+			continue;
+		}
+		settingsStream << _interface[i][0] << ":" << _interface[i][1] << "}\n";
+	}
+	return settingsStream.str();
 }
 
--- Anigma_cpp/Gear.h
+++ Anigma_cpp/Gear.h
@@ -12,7 +12,7 @@ enum gearType
 class Gear
 {
 public:
-	Gear(gearType type);
+	Gear(const std::string& filePath ,gearType type);
 	Gear(const std::string& filePath, const std::string& password);
 	~Gear();
 	//const std::string close();
@@ -20,9 +20,11 @@ public:
 	void printGearState();///function to help debuging delete when every thing works
 private:
 	//26 rows for the 26 letters first colom is input and second in output.
+	std::string settingsFile;
 	gearType _type;
 	char _interface[26][2];
 	const std::string encryptSettings(const std::string& password);
 	const std::string decryptSettings(const std::string& password);
+	const std::string getGearSettings();
 };
 
